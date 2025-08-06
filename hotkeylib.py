63b719e03b4c72b1a2a887650ba65376dbc5e50e
Magmakning1 <magmakning123456@gmail.com>
2025-08-06 19:03:29 +0200

    Add files via upload

--- hotkeylib.py
+++ hotkeylib.py
@@ -1,9 +1,20 @@
 import os
 import keyboard
 import subprocess
+import sys
+
+__all__ = ['cmd_hotkey', 'program_hotkey', 'end_hotkey_list']
+
+def _require_root():
+    if os.geteuid() != 0:
+        print("[ERROR] This program needs root privileges.")
+        sys.exit(1)
+    else:
+        print("[INFO] Running as root.")
 
 class cmd_hotkey:
     def __init__(self,key,command):
+        _require_root()
         self.key = key
         self.command = command
         keyboard.add_hotkey(str(key),lambda: os.system(str(command)))
@@ -11,6 +22,7 @@ class cmd_hotkey:
 
 class program_hotkey:
     def __init__(self,key,program):
+        _require_root()
         self.key = key
         self.program = program
         keyboard.add_hotkey(str(key),lambda: subprocess.Popen(["sudo", "-u", "#1000", str(program)]))
@@ -21,4 +33,4 @@ def end_hotkey_list():
     keyboard.wait("esc")
 
 if __name__ == "__main__":
-    print("Hotkey list not meant to be run by itself. Pleas import to another file")
+    print("Hotkey list not meant to be run by itself. Please import to another file")
