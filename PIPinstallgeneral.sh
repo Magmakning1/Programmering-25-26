5384c22020e1be3f3e9b9307fed9a5288aeb1cdb
Magmakning1 <magmakning123456@gmail.com>
2025-08-11 14:10:55 +0200

    PIPinstallgeneral.sh

--- PIPinstallgeneral.sh
+++ PIPinstallgeneral.sh
@@ -1,1 +1,25 @@
-pip3 install keyboard tk mouse pyautogui
+#!/bin/sh
+
+# if on windows
+# venv\Scripts\activate
+
+#!/bin/sh
+
+os_name=$(uname)
+
+python -m venv venv
+
+case "$os_name" in
+  Linux|Darwin)
+    source venv/bin/activate
+    ;;
+  MINGW*|MSYS*|CYGWIN*)
+    source venv/Scripts/activate
+    ;;
+  *)
+    echo "Unknown OS: $os_name"
+    exit 1
+    ;;
+esac
+
+pip install keyboard tk mouse pyautogui
